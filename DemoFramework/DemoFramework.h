89f9f025dd867550af5783b62adafb5ef17a8ac4
Mohit Kumar Singh <mohit.kumar@nickelfox.co>
2022-02-22 13:48:49 +0530

    Initial Commit

---
+++ DemoFramework/DemoFramework.h
@@ -0,0 +1,18 @@
+//
+//  DemoFramework.h
+//  DemoFramework
+//
+//  Created by Mohit Kumar Singh on 22/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoFramework.
+FOUNDATION_EXPORT double DemoFrameworkVersionNumber;
+
+//! Project version string for DemoFramework.
+FOUNDATION_EXPORT const unsigned char DemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoFramework/PublicHeader.h>
+
+
